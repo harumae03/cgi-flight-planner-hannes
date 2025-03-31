24ff06927a6fc60f2199355b2ae694266a43d365
harumae03 <hannes.arumae1@gmail.com>
2025-03-31 19:57:05 +0300

    Esimene commit peale projekti loomist

---
+++ cgi-flight-planner/src/main/java/com/cgi_flight_planner/cgi_flight_planner/CgiFlightPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.cgi_flight_planner.cgi_flight_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiFlightPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgiFlightPlannerApplication.class, args);
+	}
+
+}
---
+++ cgi-flight-planner/src/test/java/com/cgi_flight_planner/cgi_flight_planner/CgiFlightPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi_flight_planner.cgi_flight_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgiFlightPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
